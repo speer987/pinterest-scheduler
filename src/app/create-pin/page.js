a0e5ac10c6666969b9973245a19c1b1afc42ad5f
speer987 <saarah.peer15@gmail.com>
2025-05-30 13:11:57 -0700

    adding new fonts and also adding text box functionality to create pin page

--- src/app/create-pin/page.js
+++ src/app/create-pin/page.js
@@ -1,5 +1,68 @@
+import HeaderWithLinks from "../components/header-w-links";
+import Textbox from "../components/textbox";
 import "../globals.css";
 
 export default function PinCreationPage() {
-  return <div>PinCreationPage</div>;
+  return (
+    <div>
+      {/* <HeaderWithLinks selected={"create-pin"} /> */}
+      <div className="pr-64 pl-64 py-10">
+        <h1 className="text-4xl font-taviraj m-2">Create Pin</h1>
+        <div className="flex">
+          <div className="gap-2 flex flex-col justify-center items-center border-gray-300 bg-gray-50 border-1 mr-0 rounded-md w-2/6 m-2 font-condensed uppercase text-gray-700 tracking-wider">
+            <p>Upload Pin Image*</p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"
+              />
+            </svg>
+          </div>
+          <div className="w-4/6">
+            <Textbox
+              isRequired={true}
+              isLongform={false}
+              label={"Title"}
+              wordcount={100}
+            />
+            <Textbox
+              isRequired={true}
+              isLongform={true}
+              label={"Description"}
+              wordcount={800}
+            />
+            <Textbox
+              isRequired={false}
+              isLongform={true}
+              label={"Alt Text"}
+              wordcount={500}
+            />
+          </div>
+        </div>
+        <div className="flex flex-row">
+          <Textbox
+            isRequired={false}
+            isLongform={false}
+            label={"Pin Link"}
+            wordcount={500}
+          />
+          <Textbox
+            isRequired={false}
+            isLongform={false}
+            label={"Board"}
+            wordcount={500}
+          />
+        </div>
+        <input type="file"></input>
+      </div>
+    </div>
+  );
 }
